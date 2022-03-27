cd30f78c2a8e0a58cf6f72e3b335fa8ef905ad7e
Tam Nguyen <nntam2013@gmail.com>
2022-03-27 11:05:04 +0700

    feat: add announcement

---
+++ src/pages/Announcement.vue
@@ -0,0 +1,68 @@
+<script setup>
+</script>
+
+
+<template>
+  <!-- Breadcrumb Area Start -->
+  <section class="breadcrumb-area about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h2>Announcement</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Breadcrumb Area End -->
+
+  <!-- About Area Start -->
+  <section class="about-section">
+    <!-- <img loading="lazy" class="ar" src="../assets/images/about/right-image.webp" alt=""> -->
+    <img
+      loading="lazy"
+      class="s1"
+      src="../assets/images/about/s1.webp"
+      alt=""
+    />
+    <img
+      loading="lazy"
+      class="s2"
+      src="../assets/images/about/s2.webp"
+      alt=""
+    />
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="left-content">
+            <div class="section-heading">
+              <h5 class="subtitle">
+                1️⃣ We will change our project name into: Open World #openworld
+              </h5>
+              <h6 class="text">- New website: http://openworld.vision</h6>
+              <h6 class="text">
+                - Token will be changed from $xBlade into $OPEN
+              </h6>
+              <hr />
+              <h5>
+                #openworld will be our #Metaverse for people to play games,
+                build connections, make money & find happiness
+              </h5>
+              <hr />
+              <h5 class="subtitle">
+                2️⃣ Meanwhile, to play our games or buy our bonds
+              </h5>
+              <h6 class="text">Please stay with https://cryptowar.network</h6>
+              <h6 class="text">We will keep you updated! 😄🙏</h6>
+
+              <hr />
+              <h5>CryptoWar Team - soon to be OpenWorld Team 💜</h5>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+</template>
